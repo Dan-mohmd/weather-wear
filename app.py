355079a0d9877da5ef12c15dcf1f7549c162c89b
Dan-mohmd <danishcosmic26@gmail.com>
2026-06-13 00:04:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -172,7 +172,7 @@ WEATHER_CODES = {
 # -----------------------------
 class WeatherTransformer:
     def __init__(self):
-        self.api_key = "1a36b2d05745ab30a90785c8657896d7"
+        self.api_key = st.secrets["OPENWEATHER_API_KEY"]
 
         if not self.api_key:
             self.api_key = st.text_input(
